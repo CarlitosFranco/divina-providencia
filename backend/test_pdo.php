b407187f361146a15f718b186908f8c9af0027fb
Carlos Franco <krlitoz1223@gmail.com>
2026-03-27 16:32:56 -0500

    Limpieza y preparación para Render

--- backend/test_pdo.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=gondola.proxy.rlwy.net;port=57921;dbname=railway', 'root', 'CKqCxsgSedvhPNmdpeCIpnjUXPuhgtig');
-    echo "Conexión exitosa";
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
